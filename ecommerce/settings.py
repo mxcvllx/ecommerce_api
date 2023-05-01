890e26d346a086c797f694d8cc8ee918778f00c7
Odiljon11 <odiljontoirjonov350@gmail.com>
2023-05-01 15:18:35 +0500

    api

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRE T_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
