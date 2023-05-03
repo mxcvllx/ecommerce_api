eb0b5feb01e863ed8a2f7db51c1d0241ba32ce1c
Odiljon11 <odiljontoirjonov350@gmail.com>
2023-05-03 15:28:14 +0500

    end of lesson 2

--- products/views.py
+++ products/views.py
@@ -8,6 +8,5 @@ from products.serializers import ProductListSerializer
 @api_view(["GET"])
 def get_products_list(request):
     products = Product.objects.order_by("-id")
-    # products_data = [{"id": product.id, "title": product.title} for product in products]
     serializer = ProductListSerializer(products, many=True)
     return Response(serializer.data)
