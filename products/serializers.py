1a6ba8e34968cfad56c58ceff796ea4b4ecaa5cd
Odiljon11 <odiljontoirjonov350@gmail.com>
2023-05-03 15:27:31 +0500

    end of lesson 2

--- products/serializers.py
+++ products/serializers.py
@@ -1,30 +1,9 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
-from products.models import Product, Category
+from products.models import Product
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "title", "slug"]
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = Category
-        fields = ("id", "title", "position", "parent")
-
-    def update(self, instance, validated_data):
-        parent = validated_data.get("parent")
-        if instance == parent:
-            raise ValidationError("Category parent id must be different")
-        return super().update(instance, validated_data)
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        if instance.parent:
-            data["parent"] = instance.parent.title
-        return data
--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 
-from products.views import get_products_list, get_product, category, category_detail
+from products.views import get_products_list
 
 urlpatterns = [
-    # @api_view
-    path("categories/", category, name="category-list-create"),
-    path("categories/<int:pk>/", category_detail, name="category-detail"),
-    path("", get_products_list, name="products-list"),
-    # path("<int:pk>/", get_product, name="products-detail")
-    path("<slug:slug>/", get_product, name="products-detail"),
+    path("", get_products_list, name="products_list")
 ]
