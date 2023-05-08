b45d7411a55b6a8f068ac2e92958c408ef6cf67c
Odiljon11 <odiljontoirjonov350@gmail.com>
2023-05-08 12:29:36 +0500

    end of lesson 3

--- products/views.py
+++ products/views.py
@@ -1,12 +1,38 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from products.models import Product
-from products.serializers import ProductListSerializer
+from products.serializers import ProductListSerializer, ProductCreateSerializer
 
 
-@api_view(["GET"])
-def get_products_list(request):
-    products = Product.objects.order_by("-id")
-    serializer = ProductListSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return ProductCreateSerializer
+        return ProductListSerializer
+
+
+class ProductRetrieveView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+
+
+class ProductUpdateView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductCreateSerializer
+
+
+class ProductDeleteView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    # serializer_class = ProductListSerializer
+    lookup_field = "slug"
+
+    def get_serializer_class(self):
+        if self.request.method in ["PUT", "PATCH"]:
+            return ProductCreateSerializer
+        return ProductListSerializer
